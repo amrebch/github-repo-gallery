c532aca631477dba62edd687d5a9794413ba101c
Amanda C <95660057+amrebch@users.noreply.github.com>
2022-08-09 19:31:26 -0400

    Fetching my repos to the gallery

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 //profile information is in the overview div
 const overview = document.querySelector(".overview");
 const username = "amrebch";
+const repoList = document.querySelector(".repo-list");
 
 const getData = async function () {
     const response = await fetch(
@@ -27,4 +28,20 @@ const getUserInfo = function (data) {
     </div> 
     `;
     overview.append(div);
+    gitRepos();
 };
+
+const gitRepos = async function () {
+    const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await fetchRepos.json();
+    displayRepos(repoData);
+};
+
+const displayRepos = function(repos){
+    for (const repo of repos){
+      const repoItem = document.createElement("li");
+      repoItem.classList.add("repo");
+      repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+      repoList.append(repoItem);
+    }
+  };
